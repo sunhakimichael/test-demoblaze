2fb52fe45102165927bdd3d4c77b3d834d4c7629
MikeRay <michael@privyid.tech>
2026-03-06 16:55:00 +0700

    Update 3

--- config/env.config.ts
+++ config/env.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  BASE_URL: process.env.BASE_URL || "",
-  USERNAME: process.env.USERNAME || "",
-  PASSWORD: process.env.PASSWORD || "",
+  BASE_URL: process.env.BASE_URL || "https://www.demoblaze.com",
+  USERNAME: process.env.USERNAME || "your_username",
+  PASSWORD: process.env.PASSWORD || "your_password",
 };
